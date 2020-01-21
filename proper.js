10318b2b57a98e6d78e8f190d7adf3428883c655
santinobonarrigo <bonarrigosantino@gmail.com>
2020-01-20 21:31:01 -0300

    otro commit

--- proper.js
+++ proper.js
@@ -3,17 +3,17 @@ window.onload = function() {
     var pregunta1 = {
       titulo : "Cantidad de alumnos",
       opciones: [5, 15, 10],
-      correcta: 2
+      respuesta: 1
     };
     var pregunta2 = {
       titulo : "Cantidad de profes",
       opciones: [1, 47, 2],
-      correcta: 3
+      respuesta: 2
     };
     var pregunta3 = {
       titulo : "Cantidad de computradoras",
       opciones: [20, 25, 15],
-      correcta: 2
+      respuesta: 1
     };
 
 cuestionario.preguntas = [pregunta1, pregunta2, pregunta3];
@@ -23,15 +23,47 @@ cuestionario.preguntas.forEach(element => {
   titulo.innerHTML = element.titulo;
   document.body.append(titulo);
   var form = this.document.createElement("form");
-  element.opciones.forEach(correcta =>{
+  element.opciones.forEach(respuesta =>{
     var label = this.document.createElement("label");
     var input = this.document.createElement("input");
     input.setAttributte("type", "radio");
-    input.setAttributte("value", correcta);
+    input.setAttributte("value", respuesta);
     input.setAttributte("name", element.titulo);
-    label.append(correcta);
+    label.append(respuesta);
     label.appendChild(input);
     form.append(label);
   });
   document.body.append(form);
 });
+var btn = this.document.createElement("button");
+btn.innerHTML = "Validar";
+btn.onclick = function() {
+  var formularios = document.forms;
+  var respuesta1 = Array.from(formularios[0].elements).filter(x => x.checked == true);
+  var respuesta2 = Array.from(formularios[1].elements).filter(x => x.checked == true);
+  var respuesta3 = Array.from(formularios[2].elements).filter(x => x.checked == true);
+  var respuestasCorrectas = 0;
+
+  if(cuestionario.preguntas[0].respuesta == respuesta1[0].value) {
+    alert("Respuesta Correcta");
+    respuestasCorrectas ++;
+  } else {
+    alert("Seguí intentando");
+  }
+  if(cuestionario.preguntas[1].respuesta == respuesta2[0].value) {
+    alert("Respuesta Correcta");
+    respuestasCorrectas ++;
+  } else {
+    alert("Seguí intentando");
+  }
+  if(cuestionario.preguntas[2].respuesta == respuesta3[0].value) {
+    alert("Respuesta Correcta");
+    respuestasCorrectas ++;
+  } else {
+    alert("Seguí intentando");
+  }
+  if(respuestasCorrectas == 3) {
+    alert("Ganaste");
+  };
+};
+};
